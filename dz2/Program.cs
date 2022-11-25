1114c4c84d2158e5f0b2e17dc1af4e24cc054b23
yasya <kalgexavva1970@icloud.com>
2022-11-25 16:32:46 +0300

    add new file

---
+++ dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N.
+void Sumnums(int numberM,int numberN,int sum)
+{
+   if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    Sumnums(numberM, numberN, sum);
+
+}
+
+Console.Write("введите число M:");
+int numberM =int.Parse(Console.ReadLine()!);
+Console.Write("введите число N:");
+int numberN=int.Parse(Console.ReadLine()!);
+
+Sumnums(numberM,numberN,1);
