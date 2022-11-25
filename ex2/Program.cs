6f767e4b400d65edfa146fd5d0479b8b7b1f97f1
yasya <kalgexavva1970@icloud.com>
2022-11-25 16:33:01 +0300

    add new file

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+//возводит число А в целую степень B с помощью рекурсии.
+double Square(int a,int b)
+{
+    if (b==0) return 1;
+    return Square(a,b-1)*a;
+}
+Console.Write("введите число:");
+int a =int.Parse(Console.ReadLine()!);
+int b=SumDigits(a);
+Console.Write(b);
