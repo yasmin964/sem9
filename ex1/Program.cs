da75c40d8d323a065b98182cd313f27f27c86e19
yasya <kalgexavva1970@icloud.com>
2022-11-25 16:32:57 +0300

    add new file

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и
+//возвращать сумму его цифр.
+int SumDigits(int number)
+{
+    if (number==0) {return 0;}
+    int up = number/10;
+    int down=number%10;
+    return down + SumDigits(up);
+
+}
+Console.Write("введите число:");
+int a =int.Parse(Console.ReadLine()!);
+int b=SumDigits(a);
+Console.Write(b);
